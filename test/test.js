f5f8a6a84b4c0d8295789ba23899a832f2db52be
Evan Dickinson <evan_dickinson@sfu.ca>
2015-01-29 23:06:16 -0800

    Add more unit tests for stripping paragraphs.

--- test/test.js
+++ test/test.js
@@ -9,6 +9,28 @@ var md4mefi = require('../lib/md4mefi');
 
 exports['strip p tags'] = function(test) {
 	var testCases = [
+		// Don't surround plain text (no newlines) in <p> tags.
+		[
+			'Twenty bucks, same as in town.',
+			'Twenty bucks, same as in town.'
+		],
+
+
+		// Single newlines are retained.
+		// MeFi will convert them into <br> tags.
+		[
+			"Hi.\nI'm Lenny.\nThis is Carl and Homer.",
+			"Hi.\nI&apos;m Lenny.\nThis is Carl and Homer."
+		],
+
+		// Double newlines are retained.
+		// MeFi will convert them into <br><br>
+		[
+			"I like cheese.\n\nI do not like ice cream.",
+			"I like cheese.\n\nI do not like ice cream."
+		],
+
+		// Test complex paras w/ embedded tags
 		[
 			"Hello, *Wilbur*, I'm so happy to **see you**.\n\nHow's the wife?",
 			"Hello, <em>Wilbur</em>, I&apos;m so happy to <strong>see you</strong>.\n\nHow&apos;s the wife?"
